8e1ee3624eb9fcb279fbf49ef53d8d69a317bb62
vrutti lad <vruttilad2000@gmail.com>
2022-02-15 17:34:32 +0530

    Add project files.

---
+++ NullChecking/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullChecking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "welcome to the world of friends";
+            string s2 = "";
+            string s3 = string.Empty;
+
+            bool b1 = string.IsNullOrEmpty(s1);
+            bool b2 = string.IsNullOrEmpty(s2);
+            bool b3 = string.IsNullOrEmpty(s3);
+            Console.WriteLine(b1);
+            Console.ReadLine();
+            Console.WriteLine(b2);
+            Console.ReadLine();
+            Console.WriteLine(b3);
+            Console.ReadLine();
+
+        }
+    }
+}
